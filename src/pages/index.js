aa74bdb15771532ac40bbbd735dd4e407b48cd27
Hoa Nguyen Thai <hoa.nguyen-thai@lazada.com>
2019-03-08 17:24:51 +0700

    fix: title

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ const FacebookLink = styled.a`
 
 const UnderConstruction = () => (
   <Container>
-    <SEO title="Under Construction" keywords={["tiengducberlin", "underconstruction"]} />
+    <SEO title="Tiếng Đức Berlin" keywords={["tiengducberlin", "underconstruction"]} />
     <Content>
       <FacebookLink href="https://www.facebook.com/yourchoiceistrue/" title="Facebook">
         <Logo src="img/logo.jpg" />
